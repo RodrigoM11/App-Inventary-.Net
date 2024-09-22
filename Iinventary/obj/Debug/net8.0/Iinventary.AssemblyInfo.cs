8613e75fdfa13a46c285f70a4fa0552454f21aad
RodrigoM11 <rodrigogithub@hotmail.com>
2024-09-22 20:42:59 -0300

    Actualizando git y docker

--- Iinventary/obj/Debug/net8.0/Iinventary.AssemblyInfo.cs
+++ Iinventary/obj/Debug/net8.0/Iinventary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Iinventary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+247104c50c05ffddb234456f27babd5a14291938")]
 [assembly: System.Reflection.AssemblyProductAttribute("Iinventary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Iinventary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
